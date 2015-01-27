702c9d1a5874b6591f27e76fffce2d37f0bd9e80
Chris Raabe <chris@raabe.nospam>
2015-01-27 14:25:24 +0900

    Added parameter to set adaptive FAST barrier.

--- src/KeyFrame.cc
+++ src/KeyFrame.cc
@@ -158,18 +158,12 @@ KeyFrame::KeyFrame(const ATANCamera &cam)
     width /= 2; height /= 2;
   }
 
-  size_t nDesiredFeatures = 3000;
+  static gvar3<int> gvnTargetFeatureCount("Tracker.TargetFeatureCount", 3000, SILENT);
+  size_t nTargetFeatureCount = *gvnTargetFeatureCount;
   for (int i = 0; i < LEVELS; ++i) {
-    aLevels[i].SetTargetFeatureCount(nDesiredFeatures * 0.9, nDesiredFeatures * 1.1);
-    nDesiredFeatures /= 4;
+    aLevels[i].SetTargetFeatureCount(nTargetFeatureCount * 0.9, nTargetFeatureCount * 1.1);
+    nTargetFeatureCount /= 4;
   }
-
-/*
-  aLevels[0].SetTargetFeatureCount(3500, 3000);
-  aLevels[1].SetTargetFeatureCount(1000, 800);
-  aLevels[2].SetTargetFeatureCount(400, 300);
-  aLevels[3].SetTargetFeatureCount(150, 100);
-  */
 }
 
 /**
